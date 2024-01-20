c6d396d77d65c3d2c4948cd91bf63a4acef88415
Debayan <132012594+felixoder@users.noreply.github.com>
2024-01-21 01:34:15 +0530

    Update PostPage.js

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -11,7 +11,7 @@ export default function PostPage() {
   const { id } = useParams();
 
   useEffect(() => {
-    fetch(`https://mycode-space-api.vercel.app//post/${id}`)
+    fetch(`https://mycode-space-api.vercel.app/post/${id}`)
       .then((response) => response.json())
       .then((postInfo) => {
         setPostInfo(postInfo);
