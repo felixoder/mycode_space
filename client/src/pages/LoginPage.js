e1f54fc863a80435a99cc6b55c46e01a68aa9956
Debayan <132012594+felixoder@users.noreply.github.com>
2024-01-21 01:33:28 +0530

    Update LoginPage.js

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -11,7 +11,7 @@ export default function Login() {
 
   async function login(ev) {
     ev.preventDefault();
-    const response = await fetch('mycode-space-api.vercel.app/login', {
+    const response = await fetch('https://mycode-space-api.vercel.app/login', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
       headers: { 'Content-type': 'application/json' },
