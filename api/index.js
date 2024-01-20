834ddf41ceb4eb22e069d9b9155a788e069c2907
Debayan <132012594+felixoder@users.noreply.github.com>
2024-01-21 02:25:39 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
   credentials: true,
-  origin: "https://mycode-space-client.vercel.app",
+  origin: "http://localhost:3000",
 }));
 app.use(express.urlencoded({ extended: true }));
 
