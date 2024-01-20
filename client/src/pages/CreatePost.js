2bdcbf785f7f687ccfe7cf4ad9f49babef0e70e2
Debayan <132012594+felixoder@users.noreply.github.com>
2024-01-21 01:08:55 +0530

    Update CreatePost.js

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -12,7 +12,7 @@ export default function CreatePost() {
   async function createNewPost(ev) {
     ev.preventDefault();
 
-    const response = await fetch("http://localhost:4000/post", {
+    const response = await fetch("mycode-space-api.vercel.app/post", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
